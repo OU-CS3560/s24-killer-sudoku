f1edb03e0cd3a67ebcaa5fa376350eff52be43f1
NicholasAdkins <na761422@ohio.edu>
2024-02-26 01:40:52 -0500

    added 2 extra comments

--- killer-sudoku/app/Generate.tsx
+++ killer-sudoku/app/Generate.tsx
@@ -7,8 +7,6 @@
 
 import { SpaceButtonProperties, HandleHighlighting } from "./Sudoku";
 
-// Gonna work on generating the sudoku board here, this file is one big pile of TODO
-
 /**
  * @brief Initializes the board to be a 2d array, generates a board full of data with SpaceButtonProperties, and highlights the origin to start.
  * @returns A 9x9 board
@@ -34,6 +32,9 @@ export function initBoard(): SpaceButtonProperties[][] {
     // --> Take board above, its a valid Sudoku Board
     // --> Shift around certain values (rows? cols? 3x3s?) randomly such that the resulting board is still valid
     // --> Repeat as desired for a randomized full sudoku board
+    //
+    // Elsewhere, i'd say hide/erase x-number of tiles (aka show as empty), as per game difficulty
+    // After that, the board should still have at least 1 solution
 
     // Initialization Loop
     let arr: SpaceButtonProperties[][] = [];
