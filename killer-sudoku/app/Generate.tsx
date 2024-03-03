a4e8a04af285ba3349d57a4352ba12a4e17d85c4
NicholasAdkins <na761422@ohio.edu>
2024-03-02 20:08:32 -0500

    gonna eventually rework generation algorithm

--- killer-sudoku/app/Generate.tsx
+++ killer-sudoku/app/Generate.tsx
@@ -14,74 +14,10 @@ import { SpaceButtonProperties, HandleHighlighting } from "./Sudoku";
  */
 export function initBoard(): SpaceButtonProperties[][] {
 
-    /**
-     * @brief Swaps a row in (values) with a different one
-     * @param r1 row 1
-     * @param r2 row 2
-     */
-    function swapRow(r1: number, r2: number): void {
-        let temp: string[] = values[r1];
-        values[r1] = values[r2];
-        values[r2] = temp;
-    }
-
-    /**
-     * @brief Swaps a column in (values) with a different one
-     * @param c1 col 1
-     * @param c2 col 2
-     */
-    function swapCol(c1: number, c2: number): void {
-        for (let i = 0; i < 9; i++) {
-            let temp: string = values[i][c1];
-            values[i][c1] = values[i][c2];
-            values[i][c2] = temp;
-        }
-    }
-
     console.log("initBoard: Start");
     
-    let values: string[][] = [ // Start with a valid Sudoku board, shuffle it in a way that it stays valid
-        ['1','2','3',  '4','5','6',  '7','8','9'],
-        ['4','5','6',  '7','8','9',  '1','2','3'],
-        ['7','8','9',  '1','2','3',  '4','5','6'],
-
-        ['2','3','1',  '5','6','4',  '8','9','7'],
-        ['5','6','4',  '8','9','7',  '2','3','1'],
-        ['8','9','7',  '2','3','1',  '5','6','4'],
-
-        ['3','1','2',  '6','4','5',  '9','7','8'],
-        ['6','4','5',  '9','7','8',  '3','1','2'],
-        ['9','7','8',  '3','1','2',  '6','4','5']
-    ];
-
-    for (let i = 0; i < 9; i++) { // Randomly swap row/col with a different one in the same set of 3
-        let row: number = Math.floor(i/3)*3 + rand(0,2);
-        swapRow(i,row);
-        let col: number = Math.floor(i/3)*3 + rand(0,2);
-        swapCol(i,col);
-    }
-
-    for (let r3x3_1 = 0; r3x3_1 < 3; r3x3_1++) { // Randomly swap set of 3 rows/cols with a different one
-        let r3x3_2: number = rand(0,2);
-        for (let i = 0; i < 3; i++) {
-            swapRow(r3x3_1 *3 +i, r3x3_2 *3 +i);
-        }
-        r3x3_2 = rand(0,2);
-        for (let i = 0; i < 3; i++) {
-            swapCol(r3x3_1 *3 +i, r3x3_2 *3 +i);
-        }
-    }
-
-    for (let i1: number = 1; i1 <= 9; i1++) { // Randomize the placement of each set of numbers
-        let i2: number = rand(1,9);
-        for (let x = 0; x < 9; x++) {
-            for (let y = 0; y < 9; y++) {
-                if (Number(values[x][y]) == i1) {values[x][y] = i2.toString();} 
-                else 
-                if (Number(values[x][y]) == i2) {values[x][y] = i1.toString();}
-            }
-        }
-    }
+    //WIP: new board generation algorithm
+    
     console.log("initBoard: Randomization complete");
 
     // Initialization Loop, load all values onto the board's hidden data
@@ -144,3 +80,65 @@ export function initBoard(): SpaceButtonProperties[][] {
 export function rand(a: number, b: number): number {
     return Math.floor(Math.random() * (b-a+1) + a);
 };
+
+
+
+/* Tomb of the old generation algorithm (pretty fast, but wasn't random enough)
+
+    function swapRow(r1: number, r2: number): void {
+        let temp: string[] = values[r1];
+        values[r1] = values[r2];
+        values[r2] = temp;
+    }
+    
+    function swapCol(c1: number, c2: number): void {
+        for (let i = 0; i < 9; i++) {
+            let temp: string = values[i][c1];
+            values[i][c1] = values[i][c2];
+            values[i][c2] = temp;
+        }
+    }
+
+    let values: string[][] = [ // Start with a valid Sudoku board, shuffle it in a way that it stays valid
+        ['1','2','3',  '4','5','6',  '7','8','9'],
+        ['4','5','6',  '7','8','9',  '1','2','3'],
+        ['7','8','9',  '1','2','3',  '4','5','6'],
+
+        ['2','3','1',  '5','6','4',  '8','9','7'],
+        ['5','6','4',  '8','9','7',  '2','3','1'],
+        ['8','9','7',  '2','3','1',  '5','6','4'],
+
+        ['3','1','2',  '6','4','5',  '9','7','8'],
+        ['6','4','5',  '9','7','8',  '3','1','2'],
+        ['9','7','8',  '3','1','2',  '6','4','5']
+    ];
+
+    for (let i = 0; i < 9; i++) { // Randomly swap row/col with a different one in the same set of 3
+        let row: number = Math.floor(i/3)*3 + rand(0,2);
+        swapRow(i,row);
+        let col: number = Math.floor(i/3)*3 + rand(0,2);
+        swapCol(i,col);
+    }
+
+    for (let r3x3_1 = 0; r3x3_1 < 3; r3x3_1++) { // Randomly swap set of 3 rows/cols with a different one
+        let r3x3_2: number = rand(0,2);
+        for (let i = 0; i < 3; i++) {
+            swapRow(r3x3_1 *3 +i, r3x3_2 *3 +i);
+        }
+        r3x3_2 = rand(0,2);
+        for (let i = 0; i < 3; i++) {
+            swapCol(r3x3_1 *3 +i, r3x3_2 *3 +i);
+        }
+    }
+
+    for (let i1: number = 1; i1 <= 9; i1++) { // Randomize the placement of each set of numbers
+        let i2: number = rand(1,9);
+        for (let x = 0; x < 9; x++) {
+            for (let y = 0; y < 9; y++) {
+                if (Number(values[x][y]) == i1) {values[x][y] = i2.toString();} 
+                else 
+                if (Number(values[x][y]) == i2) {values[x][y] = i1.toString();}
+            }
+        }
+    }
+*/
