739ffcc5b62a020d85a423d2fb5ec6f47a04c282
NicholasAdkins <na761422@ohio.edu>
2024-03-31 18:03:01 -0400

    updated static-analysis to incorporate new changes elsewhere & merged the .cjs & .json files

--- killer-sudoku/.eslintrc.cjs
+++ killer-sudoku/.eslintrc.cjs
@@ -1,11 +1,17 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-    ],
-  };
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals' // <-- moved here from .eslintrc.json
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'prefer-const': 'warn',
+    'no-constant-condition': 'warn'
+  }
+};
