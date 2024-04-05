0f36eaa69e0e21fbc8f0bc3e884fa0bdceace0a2
nickadkins47 <na761422@ohio.edu>
2024-04-05 17:45:25 -0400

    more WIP changes in GenKiller

--- killer-sudoku/app/GenKiller.tsx
+++ killer-sudoku/app/GenKiller.tsx
@@ -14,7 +14,8 @@ const kKey: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 /** 
  * @brief data type for containing information abt a killer sudoku group
  * @member sum sum of all numbers in this group
- * @member size number of tiles in this group
+ * @member curSize number of tiles in this group
+ * @member maxSize (WIP)
  * @member symbol unique char for this group, mainly to identify
  *  the differences between each group (aka all tiles of the same group
  *  will have the same symbol attached to it)
@@ -25,7 +26,8 @@ const kKey: string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 */
 type kTile = {
     sum: number,
-    size: number,
+    curSize: number,
+    maxSize?: number,
     symbol: string
 };
 
@@ -35,62 +37,99 @@ type kTile = {
  * @return None
 */
 export function genKiller(board: SpaceButtonProperties[][]): void {
+
+    //TODO: rework this so that it is based on difficulty
+    //Percent Configuration, switch around values
+    const percent: {[n: number]: number} = {
+        1: 15,
+        2: 35,
+        3: 35,
+        4: 15,
+        5: 0
+
+    };
+
     //checks if coords (x,y) are within board coordinates
     const onBoard = (x: number, y: number): boolean => {
         return ((0 <= x && x <= 8) && (0 <= y && y <= 8));
     }
 
-    //# of killer groups (vary on difficulty?)
-    const AmountTotal: number = 32;
-
-    let groups: kTile[][] = [];
+    //Initialize killer group grid
+    let arr: kTile[][] = [];
     for (let i = 0; i < 9; i++) {
-        groups[i] = [];
+        arr[i] = [];
         for (let j = 0; j < 9; j++) {
-            groups[i][j] = {sum: Number(board[i][j].hiddenData), size: 1, symbol: '.'};
+            arr[i][j] = {sum: Number(board[i][j].hiddenData), curSize: 1, symbol: '.'};
         }
     }
 
     //Puts (#) of tiles into initial groups, no picking already chosen tiles or tiles next to those
-    let numTaken = 0;
-    for (let i = 0; i < AmountTotal; i++) {
-        if (numTaken == 81) break;
+    let numGroups: number = 0;
+    for (let numOcc = 0; numOcc < 81;) {
         let x = 0, y = 0;
         do {
             x = rand(0,8); y = rand(0,8);
-        } while (groups[x][y].symbol != '.');
-        groups[x][y].symbol = kKey[i];
-        numTaken++;
-        for (let neighbors of [[x,y-1],[x+1,y],[x,y+1],[x-1,y]]) {
-            const x0 = neighbors[0], y0 = neighbors[1];
+        } while (arr[x][y].symbol != '.');
+        arr[x][y].symbol = kKey[numGroups];
+        numOcc++; numGroups++;
+        const borders: [number,number][] = [[x,y-1],[x+1,y],[x,y+1],[x-1,y]];
+        for (let [x0,y0] of borders) {
             if (!onBoard(x0,y0)) continue;
-            if (groups[x0][y0].symbol == '.') {groups[x0][y0].symbol = '/'; numTaken++;}
+            if (arr[x0][y0].symbol == '.') {arr[x0][y0].symbol = '/'; numOcc++;}
+        }
+    }
+
+    //attach a maximum group size for each group on the board
+    for (let row of arr) {
+        for (let val of row) {
+            if (val.symbol == '.' || val.symbol == '/') continue;
+            const rNum = Math.random()*100;
+            if (rNum > 85)
         }
     }
 
     //for each initial group, add bordering tiles until no blanks left
-    for (let numBlank = 81-AmountTotal; numBlank > 0;) {
+    for (let numBlank = 81-numGroups; numBlank > 0;) {
         for (let x = 0; x < 9; x++) {
             for (let y = 0; y < 9; y++) {
-                if (groups[x][y].symbol != '.' || groups[x][y].symbol != '/') continue;
+                if (arr[x][y].symbol == '.' || arr[x][y].symbol == '/') continue;
+
+            }
+        }
+    }
+
+    /*
+                if (groups[x][y].symbol != '.' && groups[x][y].symbol != '/') continue;
                 let neighbors: [number,number][] = [];
                 for (let opt of [[x,y-1],[x+1,y],[x,y+1],[x-1,y]]) {
                     const x0 = opt[0], y0 = opt[1];
                     if (!onBoard(x0,y0)) continue;
                     if (groups[x0][y0].symbol == '.' || groups[x0][y0].symbol == '/') continue;
-                    neighbors.push([x0,y0]);
+                    if (groups[x0][y0].size < sizeLimit) neighbors.push([x0,y0]);
                 }
-                if (neighbors.length == 0) continue;
-                //sort possible options by group size: smaller groups -> higher priority
-                neighbors.sort((a,b)=>{return groups[a[0]][a[1]].size-groups[b[0]][b[1]].size});
-                const [a,b] = neighbors[0];
-                groups[a][b].size++;
-                groups[a][b].sum += groups[x][y].sum;
-                groups[x][y] = groups[a][b]; //merged, both locations now point to same tile/info
+                const neiLen = neighbors.length;
+                if (neiLen == 0) continue;
+
+                for (let i = 0; i < neiLen; i++) {
+                    const j = rand(0,neiLen-1);
+                    const temp = neighbors[i];
+                    neighbors[i] = neighbors[j];
+                    neighbors[j] = temp;
+                }
+
+                let numNeighborsSzG4 = 0;
+                for (let [a,b] of neighbors) {
+                    if (groups[a][b].size >= sizeLimit-1) {numNeighborsSzG4++; continue;}
+                    groups[a][b].size++;
+                    groups[a][b].sum += groups[x][y].sum;
+                    groups[x][y] = groups[a][b]; //merged, both locations now point to same tile/info
+                    break;
+                }
+                if (numNeighborsSzG4 == neiLen) {
+                    groups[x][y].symbol = kKey[numGroups]; numGroups++;
+                }
                 numBlank--;
-            }
-        }
-    }
+    */
 
     let keyTrack: {[i: string]: boolean} = {};
     for (let char of kKey) {
@@ -98,10 +137,10 @@ export function genKiller(board: SpaceButtonProperties[][]): void {
     }
     for (let y = 0; y < 9; y++) {
         for (let x = 0; x < 9; x++) {
-            const char = groups[x][y].symbol;
+            const char = arr[x][y].symbol;
             if (keyTrack[char] == false) {
                 keyTrack[char] = true;
-                board[x][y].topLeftNumber = groups[x][y].sum;
+                board[x][y].topLeftNumber = arr[x][y].curSize;
             }
         }
     }
@@ -121,10 +160,10 @@ export function genKiller(board: SpaceButtonProperties[][]): void {
         for (let y = 0; y < 9; y++) {
             board[x][y].mutableStatus = 'dashedBorder0000'; //Need to do this for some reason
             let neighbors: string[] = [];
-            const thisSym: string = groups[x][y].symbol;
+            const thisSym: string = arr[x][y].symbol;
             for (let opt of [[x,y-1],[x+1,y],[x,y+1],[x-1,y]]) {
                 const x0 = opt[0], y0 = opt[1];
-                neighbors.push((onBoard(x0,y0) && groups[x0][y0].symbol == thisSym) ? '1' : '0');
+                neighbors.push((onBoard(x0,y0) && arr[x0][y0].symbol == thisSym) ? '1' : '0');
             }
             board[x][y].mutableStatus = `dashedBorder${neighbors.join('')}`;
         }
