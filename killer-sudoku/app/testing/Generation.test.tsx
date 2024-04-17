60956e665bce5893d80923c6f588312b1996bf00
nickadkins47 <na761422@ohio.edu>
2024-04-17 11:00:55 -0400

    added file for my future tests (WIP)

---
+++ killer-sudoku/app/testing/Generation.test.tsx
@@ -0,0 +1,24 @@
+/**
+ * @file     Generation.test.tsx
+ * @author   Nicholas Adkins <na761422@ohio.edu>
+ * @brief    Testing File for Generation stuff
+ * @date     April 17, 2024
+*/
+
+import { describe, expect, test } from "@jest/globals";
+import { initBoard } from "../Generate";
+
+//TODO: Check if newly generated board
+// is both valid & occ == 81
+describe("", ()=>{
+    test("WIP", ()=>{
+        expect(1).toBe(1);
+    });
+});
+
+
+//TODO: Check if tile hiding system hides
+// the correct number of tiles
+
+//TODO: Check if killer group generation
+// does not have any groups over size 5
