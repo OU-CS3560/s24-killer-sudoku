14752277fbd7037643f38adc103e2751ae84a64a
Drew Mullett <dm247120@ohio.edu>
2024-04-17 15:33:57 -0400

    Got tests running after merge with main

---
+++ killer-sudoku/app/testing/SpaceButtonProperties.ts
@@ -0,0 +1,12 @@
+export interface SpaceButtonProperties {
+    data: string,
+    highlighted: string,
+    savedData: string,
+    hiddenData: string,
+    fixedStatus: string,
+    mutableStatus: string,
+    locked: boolean,
+    previousHighlight: string,
+    marked: boolean,
+    topLeftNumber: number,
+};
